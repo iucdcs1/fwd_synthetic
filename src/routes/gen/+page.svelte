c28aa21eee45c17c4ec8cceec703bd14599ff227
VikaLa7 <7322vipgmail.com>
2023-07-19 21:34:51 +0300

    home page

--- src/routes/gen/+page.svelte
+++ src/routes/gen/+page.svelte
@@ -2,8 +2,8 @@
 
 </script>
 
-    <div class="amount__form-container">
-        <form class="amount__form">
+    <div class="gen__form-container">
+        <form class="gen__form">
             <h2>Amount:</h2>
             <input type="text" id="amount" name="amount" placeholder="Amount" required>
         </form>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<div class="home__form-container">
+        <div class="home__title">
+            <h2 class="home__tabletext">Table:</h2>
+            <form>
+            <input type="text" id="home__name" name="home__name" required>
+            </form>
+        </div>
+        <input type="move" value="Gen" class="home__button">
+</div>
