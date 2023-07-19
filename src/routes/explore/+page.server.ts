781a0393c5fab3aa95e11ff8749d97624ed1e61b
Senopiece <shichiromail@gmail.com>
2023-07-19 19:07:52 +0300

    fix datatype mismatch

--- src/routes/explore/+page.server.ts
+++ src/routes/explore/+page.server.ts
@@ -1,5 +1,6 @@
 import { Client } from 'pg';
 import type { FetchResult, FetchError } from "./interfaces";
+import type { ColumnInfo } from "../interfaces";
 
 export async function load({ url }): Promise<FetchResult | FetchError> {
     const dbLink = url.searchParams.get('dbLink');
@@ -18,8 +19,6 @@ export async function load({ url }): Promise<FetchResult | FetchError> {
     try {
         await client.connect();
 
-        // TODO: apply developed scripts here to populate the TableInfo datatype, if needed datatype can be extended
-
         const res = await client.query(`
             SELECT table_name
             FROM information_schema.tables
@@ -36,14 +35,46 @@ export async function load({ url }): Promise<FetchResult | FetchError> {
         for (const tableName of tableNames) {
             const columnsRes = await client.query({
                 text: `
-                    SELECT column_name
+                    SELECT column_name, data_type
                     FROM information_schema.columns
                     WHERE table_schema = 'public'
                     AND table_name = $1;
                 `,
                 values: [tableName]
             });
-            const columns = columnsRes.rows.map((row) => row.column_name);
+            const columns: ColumnInfo[] = columnsRes.rows.map((row) => {
+                const column: ColumnInfo = {
+                    name: row.column_name,
+                    type: row.data_type,
+                    primaryKey: false,
+                    foreignKey: false,
+                };
+                return column;
+            });
+
+            const keysRes = await client.query({
+                text: `
+                    SELECT column_name, constraint_type
+                    FROM information_schema.table_constraints tc
+                    JOIN information_schema.constraint_column_usage ccu
+                    USING (constraint_schema, constraint_name)
+                    WHERE tc.table_schema = 'public'
+                    AND tc.table_name = $1;
+                `,
+                values: [tableName]
+            });
+
+            for (const keyRow of keysRes.rows) {
+                const column = columns.find((col) => col.name === keyRow.column_name);
+                if (column) {
+                    if (keyRow.constraint_type === 'PRIMARY KEY') {
+                        column.primaryKey = true;
+                    } else if (keyRow.constraint_type === 'FOREIGN KEY') {
+                        column.foreignKey = true;
+                    }
+                }
+            }
+
             args.tables.push({ name: tableName, columns: columns });
         }
 
