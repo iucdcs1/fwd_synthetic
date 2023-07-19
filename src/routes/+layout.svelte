be9d326dad1d5d78d9621524ca1139d0f076b8da
Senopiece <shichiromail@gmail.com>
2023-07-19 17:32:16 +0300

    fix incorrect import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-    import Header from "../lib/components/Header.svelte";
-    import Footer from "../lib/components/Footer.svelte";
-    import * as css from "../../static/css/style.css";
+	import Header from '../lib/components/Header.svelte';
+	import Footer from '../lib/components/Footer.svelte';
 </script>
-
 
 <Header />
 <main>
-    <slot />
+	<slot />
 </main>
 <Footer />
-
