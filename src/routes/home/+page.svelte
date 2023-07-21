bd36a36a49c4fb2977406919dc8f7c76de19ddb4
Artem <artem.volkonitin@yandex.ru>
2023-07-22 01:18:33 +0300

    Changed logic in selecting tables, added new field into the TableData interface

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,37 +1,122 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
+    interface Column {
+        column_name: string;
+        column_default: string | null;
+        data_type: string;
+        is_nullable: string;
+        character_maximum_length: number | null;
+        is_generated: string;
+        related_to?: RelatedColumn[];
+    }
+
+    interface RelatedColumn {
+        table_name: string;
+        column_name: string;
+    }
+
+    interface TableData {
+        table_name: string;
+        columns: {
+            [key: string]: Column;
+        };
+        isEmpty: boolean;
+    }
+
     interface Data {
         tableName: string[];
     }
 
-    onMount(async () => {
+    let selectedTableName = '';
+    let tableNames: string[] = [];
+    let tableData: TableData | null = null;
+
+    const fetchData = async () => {
+        try {
+            if (selectedTableName == ''){
+                tableData.table_name = '';
+                tableData.isEmpty = true;
+                tableData.columns = null;
+            } else {
+            const response = await fetch(`/json/${selectedTableName}.json`);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch ${selectedTableName}.json`);
+            }
+            tableData = await response.json();
+            tableData.isEmpty = false;
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchTableNames = async () => {
         try {
             const response = await fetch('/json/data.json');
             if (!response.ok) {
                 throw new Error('Failed to fetch data.json');
             }
             const data: Data = await response.json();
-
-            const tableData = data.tableName;
-            const selectList = document.getElementById('home__listselect') as HTMLSelectElement;
-
-            tableData.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item;
-                option.text = item;
-                selectList.appendChild(option);
-            });
+            tableNames = data.tableName;
         } catch (error) {
             console.error(error);
         }
+    };
+
+    onMount(() => {
+        fetchTableNames();
+        fetchData();
     });
+
+    const handleTableSelect = () => {
+        fetchData();
+    };
 </script>
 
 <div class="home__form-container">
     <div class="home__title">
         <h2 class="home__tabletext">Table:</h2>
-        <select id="home__listselect"></select>
+        <select id="home__listselect" bind:value={selectedTableName} on:change={handleTableSelect}>
+            <option value="">Select a table</option>
+            {#each tableNames as tableName}
+                <option value={tableName}>{tableName}</option>
+            {/each}
+        </select>
     </div>
-    <input type="move" value="Gen" class="home__button">
+    {#if tableData && !tableData.isEmpty}
+        <h3>{tableData.table_name}</h3>
+        <table>
+            <thead>
+            <tr>
+                {#each Object.keys(tableData.columns) as columnName}
+                    <th>{columnName}</th>
+                {/each}
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                {#each Object.values(tableData.columns) as column}
+                    <td>
+                        <div>{column.column_name}</div>
+                        <div>{column.column_default}</div>
+                        <div>{column.data_type}</div>
+                        <div>{column.is_nullable}</div>
+                        <div>{column.character_maximum_length}</div>
+                        <div>{column.is_generated}</div>
+                        {#if column.related_to}
+                            <ul>
+                                {#each column.related_to as relatedColumn}
+                                    <li>{relatedColumn.table_name} - {relatedColumn.column_name}</li>
+                                {/each}
+                            </ul>
+                        {/if}
+                    </td>
+                {/each}
+            </tr>
+            </tbody>
+        </table>
+    {:else}
+        <p>No data available</p>
+    {/if}
 </div>
