a33787900da226cb439fd8384c4dfd613f1c5019
Senopiece <shichiromail@gmail.com>
2023-07-24 15:04:27 +0300

    load saved last acessed table

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -83,15 +83,18 @@
 		});
 	}
 
+	let tableData: TableData | undefined;
 	let selectedTableName = '';
 	const unsubscribe = appState.subscribe((currentState) => {
 		selectedTableName = currentState.selected_table;
+		if ('dbLink' in data) {
+			tableData = data.tables.find((value, index, obj) => value.table_name == selectedTableName);
+		}
 	});
 	onDestroy(() => {
 		unsubscribe();
 	});
 
-	let tableData: TableData | undefined;
 	const handleTableSelect = () => {
 		if ('dbLink' in data) {
 			tableData = data.tables.find((value, index, obj) => value.table_name == selectedTableName);
@@ -106,79 +109,83 @@
 
 <!-- TODO: requires to rename all the home__ entries -->
 <div class="home__form-container">
-	{#if 'dbLink' in data}
-		<div class="home__title">
-			<h2 class="home__tabletext">Table:</h2>
-			<select id="home__listselect" bind:value={selectedTableName} on:change={handleTableSelect}>
-				<option value="">Select a table</option>
-				{#each data.tables as table}
-					<option value={table.table_name}>{table.table_name}</option>
-				{/each}
-			</select>
-		</div>
-		{#if tableData !== undefined}
-			<h3 class="home__tablename">{tableData?.table_name}</h3>
-			<div class="home__table">
-				<table>
-					<thead>
-						<tr>
-							<!-- Add a new static column for the fields -->
-							<th>column_name</th>
-							<th>column_default</th>
-							<th>data_type</th>
-							<th>is_nullable</th>
-							<th>character_maximum_length</th>
-							<th>is_generated</th>
-							<th>is_gen_chosen</th>
-						</tr>
-					</thead>
-					<tbody>
-						{#each Object.values(tableData.columns) as column}
-							<tr>
-								<td>
-									<div>
-										<button class="colbtn" on:click={() => gotoDetails(column.column_name)}
-											>{column.column_name}</button
-										>
-									</div>
-								</td>
-								<td>
-									<div>{column.column_default}</div>
-								</td>
-								<td>
-									<div>{column.data_type}</div>
-								</td>
-								<td>
-									<div>{column.is_nullable}</div>
-								</td>
-								<td>
-									<div>{column.character_maximum_length}</div>
-								</td>
-								<td>
-									<div>{column.is_generated}</div>
-									{#if column.related_to}
-										{#each column.related_to as relatedColumn}
-											<ul>
-												<li>{relatedColumn.table_name} - {relatedColumn.column_name}</li>
-											</ul>
-										{/each}
-									{/if}
-								</td>
-								<td>
-									<div>{column.generator !== undefined}</div>
-								</td>
-							</tr>
-						{/each}
-					</tbody>
-				</table>
+	{#if isBrowser}
+		{#if 'dbLink' in data}
+			<div class="home__title">
+				<h2 class="home__tabletext">Table:</h2>
+				<select id="home__listselect" bind:value={selectedTableName} on:change={handleTableSelect}>
+					<option value="">Select a table</option>
+					{#each data.tables as table}
+						<option value={table.table_name}>{table.table_name}</option>
+					{/each}
+				</select>
 			</div>
+			{#if tableData !== undefined}
+				<h3 class="home__tablename">{tableData?.table_name}</h3>
+				<div class="home__table">
+					<table>
+						<thead>
+							<tr>
+								<!-- Add a new static column for the fields -->
+								<th>column_name</th>
+								<th>column_default</th>
+								<th>data_type</th>
+								<th>is_nullable</th>
+								<th>character_maximum_length</th>
+								<th>is_generated</th>
+								<th>is_gen_chosen</th>
+							</tr>
+						</thead>
+						<tbody>
+							{#each Object.values(tableData.columns) as column}
+								<tr>
+									<td>
+										<div>
+											<button class="colbtn" on:click={() => gotoDetails(column.column_name)}
+												>{column.column_name}</button
+											>
+										</div>
+									</td>
+									<td>
+										<div>{column.column_default}</div>
+									</td>
+									<td>
+										<div>{column.data_type}</div>
+									</td>
+									<td>
+										<div>{column.is_nullable}</div>
+									</td>
+									<td>
+										<div>{column.character_maximum_length}</div>
+									</td>
+									<td>
+										<div>{column.is_generated}</div>
+										{#if column.related_to}
+											{#each column.related_to as relatedColumn}
+												<ul>
+													<li>{relatedColumn.table_name} - {relatedColumn.column_name}</li>
+												</ul>
+											{/each}
+										{/if}
+									</td>
+									<td>
+										<div>{column.generator !== undefined}</div>
+									</td>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</div>
+			{:else}
+				<p>No table</p>
+			{/if}
+			<button class="home__button">Gen</button>
 		{:else}
-			<p>No table</p>
+			<p>{data.err}</p>
+			<p>{data.detail}</p>
 		{/if}
-		<button class="home__button">Gen</button>
 	{:else}
-		<p>{data.err}</p>
-		<p>{data.detail}</p>
+		<p>Loading...</p>
 	{/if}
 </div>
 
